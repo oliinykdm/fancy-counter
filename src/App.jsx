53a1cc40eeb1d4796082bef0e31365bf45f1a0d3
Dmytro Oliinyk <dmytro.oliinyk@easylearn.ch>
2024-07-16 11:57:16 +0200

    Init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Count from "./Count";
+import CountButtons from "./CountButtons";
+import ResetButton from "./ResetButton";
+import Title from "./Title";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <Title />
+        <Count />
+        <ResetButton />
+        <CountButtons />
+      </div>
+    </main>
+  );
+}
+
+export default App;
